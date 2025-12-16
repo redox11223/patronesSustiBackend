f47f7e3a32daa66da63361cb8739138a229a1553
Miguel Angel <miguelgarcia241296@gmail.com>
2025-12-16 12:56:47 -0500

    feat: implemnte los servicios basicos de Spring security

--- src/main/java/com/patrones/susti/auth/MiUserDetailsService.java
+++ src/main/java/com/patrones/susti/auth/MiUserDetailsService.java
@@ -1,4 +1,40 @@
 package com.patrones.susti.auth;
 
-public class MiUserDetailsService {
+import com.patrones.susti.usuarios.Usuario;
+import com.patrones.susti.usuarios.UsuarioRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MiUserDetailsService implements UserDetailsService {
+
+  private final UsuarioRepo usuarioRepo;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Usuario usuario =usuarioRepo.findByNombre(username).orElseThrow(()->
+        new UsernameNotFoundException("Usuario no encontrado: " + username));
+
+    GrantedAuthority authority=new SimpleGrantedAuthority(usuario.getRoles().getNombre());
+    List<GrantedAuthority> authorities=List.of(authority);
+
+    return new User(
+        usuario.getNombre(),
+        usuario.getConstrasena(),
+        usuario.isActivo(),
+        true,
+        true,
+        true,
+        authorities
+    );
+  }
 }
--- src/main/java/com/patrones/susti/config/SecurityConfig.java
+++ src/main/java/com/patrones/susti/config/SecurityConfig.java
@@ -1,4 +1,37 @@
 package com.patrones.susti.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+            .csrf(csrf-> csrf.disable())
+            .authorizeHttpRequests(authorize -> authorize
+            .anyRequest().permitAll()
+        );
+
+    return http.build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
 }
